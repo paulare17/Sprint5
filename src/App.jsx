23a296614c52948b654447ce7c1b99783ca476d1
Paula <paulare17@gmail.com>
2025-04-01 19:10:23 +0200

    Exercici 3, maquetació feta

--- src/App.jsx
+++ src/App.jsx
@@ -9,32 +9,38 @@ function App() {
     {
       title: "Dedica molt de temps",
       description: "Un mínim de 30 hores a la setmana. Si no en tens prou, hauràs de dedicar-li més hores. Al principi sembla impossible, però notaras una millora ràpidament",
-      bgColor: "",
+      bgColor: "color1",
       image: 'src/assets/time_managment.svg'
     },
     {
       title: "Programa projectes propis",
       description: "Més val 10 hores treballant en projectes propis, que 10 hores mirant tutorials. La motivació i la implicació en el projecte ajudarà a accelerar el teu aprenentatge.",
-      bgColor: "",
+      bgColor: "color2",
       image: 'src/assets/programming.svg'
     },
     {
       title: "Procura descansar",
       description: "Descansar bé i desconnectar són vitals. D'aquesta manera reduiràs l'estrès i l'ansietat. Milloraràs la teva concentració i consolidaras el teu aprenentatge.",
-      bgColor: "",
+      bgColor: "color3",
       image: 'src/assets/meditation.svg',
     },
   ];
 
   const [step, setStep] = useState(0);
 
+  function nextStep() {
+    setStep(prev => (prev + 1) % tutorialData.length )
+  }
+
 
   return (
     <>
     <Card 
-    image= {tutorialData[step].image}
-    title= {tutorialData[step].title}
-    description={tutorialData[step].description}
+    image = {tutorialData[step].image}
+    title = {tutorialData[step].title}
+    description = {tutorialData[step].description}
+    bgColor = {tutorialData[step].bgColor}
+    nextStep = {nextStep}
     />
      
     </>
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,11 +1,23 @@
-export default function Card({title, description, image}) {
+export default function Card({title, description, image, bgColor, nextStep}) {
     return (
-        <div className="card">
-            <div className="card-body">
-                <img src={image}></img>
-            <h1> {title}</h1>
+        <div className={`card-container `}>
+        <div className={`card `}>
+          <div className={`card-header ${bgColor}`}>
+            <img src={image} alt={title} />
+          </div>
+          <div className="card-body">
+            <h2>{title}</h2>
             <p>{description}</p>
+            <div className="card-footer">
+              <div className="dots">
+                <span className="dot active"></span>
+                <span className="dot"></span>
+                <span className="dot"></span>
+              </div>
+              <button onClick={nextStep} className="btn btn-dark rounded-circle">➝</button>
             </div>
+          </div>
         </div>
+      </div>
     )
 }
