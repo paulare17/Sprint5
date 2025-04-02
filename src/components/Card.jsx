9d994889f9d33044dc77a71429b163c24ed19671
Paula <paulare17@gmail.com>
2025-04-02 10:28:21 +0200

    Exercici 4 finalitzat

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -10,9 +10,9 @@ export default function Card({title, description, image, bgColor, step, nextStep
             <p>{description}</p>
             <div className="card-footer">
               <div className="dots">
-                <span className="dot active"></span>
-                <span className="dot"></span>
-                <span className="dot"></span>
+                <span className={`${step === 0 ? 'dot active' : 'dot'}`}></span>
+                <span className={`${step === 1 ? 'dot active' : 'dot'}`}></span>
+                <span className={`${step === 2 ? 'dot active' : 'dot'}`}></span>
               </div>
               <button onClick={prevStep} className = {`btn btn-light rounded-circle ${step === 0 ? 'd-none' : ''}`}>←</button>
               <button onClick={nextStep} className={`btn btn-dark rounded-circle ${step === 2 ? 'd-none' : ''}`}>→</button>
